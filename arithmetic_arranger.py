d507958c1666d8a5a1325bce547eb40f3612e9e7
yhungp <47538388+yhungp@users.noreply.github.com>
2022-04-05 08:25:09 -0400

    Update arithmetic_arranger.py
    - fixed that subtraction operations had a positive sign
    - fixed the problem that it did not show the equation when passing it a False parameter instead of just not showing the result

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,4 @@
-def arithmetic_arranger(l: list, show: bool):
+def arithmetic_arranger(l: list, show: bool = False):
     if l.__len__() > 5: return "Error: Too many problems."
 
     solutions = []
@@ -11,7 +11,7 @@ def arithmetic_arranger(l: list, show: bool):
             if op[0].__len__() > 4 or op[1].__len__() > 4: return "Error: Numbers cannot be more than four digits."
 
             result = int(op[0]) + int(op[1])
-            solutions.append(formatter(op[0], op[1], result, "+"))
+            solutions.append(formatter(op[0], op[1], result, "+", show))
 
         elif "-" in op:
             op = str(op).replace(" ", "").split("-")
@@ -20,24 +20,27 @@ def arithmetic_arranger(l: list, show: bool):
             if op[0].__len__() > 4 or op[1].__len__() > 4: return "Error: Numbers cannot be more than four digits."
             
             result = int(op[0]) - int(op[1])
-            solutions.append(formatter(op[0], op[1], result, "+"))
+            solutions.append(formatter(op[0], op[1], result, "-", show))
 
         elif "*" in op or "/" in op:
             return "Error: Operator must be '+' or '-'."
 
-    solutions = ["    ".join([ele[i] for ele in solutions] )for i in range(4) ]
+    if show: l = 4
+    else: l = 3
+
+    solutions = ["    ".join([ele[i] for ele in solutions] )for i in range(l) ]
     solutions = "\n".join(solutions)
 
-    if show: print(solutions)
+    print(solutions)
 
     return solutions
 
         
-def formatter(val1, val2, result, operation):
-    if str(val1) >= str(val2):
-        width = str(val1).__len__() + 2
+def formatter(val1, val2, result, operation, show):
+    if len(str(val1)) >= len(str(val2)):
+        width = len(str(val1)) + 2
     else:
-        width = str(val2).__len__() + 2
+        width = len(str(val2)) + 2
 
     line1 = " " * ( width - len(str(val1))) + str(val1)
     line2 = operation + " " * ( width - len(str(val2)) - 1) + str(val2)
@@ -46,4 +49,5 @@ def formatter(val1, val2, result, operation):
     if result >= 0: line4 = " " * ( width - len(str(result))) + str(result)
     else: line4 = " " * ( width - len(str(result))) + str(result)
 
-    return [line1, line2, line3, line4]
+    if show: return [line1, line2, line3, line4]
+    else: return [line1, line2, line3]
